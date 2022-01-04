832f7f3692158911f38b2cd5f0ed00f59eabfa88
Andy Kent <andy@binaryalchemy.net>
2022-01-04 13:00:14 +0000

    Initial Commit

---
+++ PortalAudioDemo/PortalAudioDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortalAudioDemoApp.swift
+//  PortalAudioDemo
+//
+//  Created by Andy Kent on 04/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct PortalAudioDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
